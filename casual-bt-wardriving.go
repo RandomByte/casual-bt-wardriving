7c00206fb4c38b7d5dee24f194f9a90395b3586c
RandomByte <r@ndombyte.net>
2017-06-04 22:48:55 +0200

    Basic scan

--- casual-bt-wardriving.go
+++ casual-bt-wardriving.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+var re = regexp.MustCompile("(?im)^[^0-9a-f]*((?:[0-9a-f]{2}:){5}[0-9a-f]{2})\\s*([^\\s].*)$")
+
+func printCommand(cmd *exec.Cmd) {
+	fmt.Printf("==> Executing: %s\n", strings.Join(cmd.Args, " "))
+}
+
+func printError(err error) {
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
+	}
+}
+
+func printOutput(outs []byte) {
+	if len(outs) > 0 {
+		fmt.Printf("==> Output: %s\n", string(outs))
+	}
+}
 
 func main() {
-    fmt.Printf("Hello, world.\n")
+
+	// cmdOutput.WriteString("Scanning ...\n	00:26:4A:A3:23:3C	Ares\n	11:26:4A:A3:23:3C	Ares2")
+	parse(scan())
+}
+
+func scan() string {
+	// Create an *exec.Cmd
+	cmd := exec.Command("hcitool", "scan", "--flush")
+
+	// Stdout buffer
+	cmdOutput := &bytes.Buffer{}
+	// Attach buffer to command
+	cmd.Stdout = cmdOutput
+
+	err := cmd.Run() // will wait for command to return
+	printError(err)
+	return cmdOutput.String()
+}
+
+func parse(rawScanResult string) {
+	addresses := re.FindAllStringSubmatch(rawScanResult, -1)
+	for _, addr := range addresses {
+		mac := addr[1]
+		name := addr[2]
+
+		fmt.Printf("MAC: %s - Name: %s\n", mac, name)
+	}
 }
---
+++ casual-bt-wardriving_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	parse("Scanning ...\n	12:34:56:78:90:42	Device 1\n	13:37:13:37:13:37	//Device $('2")
+}
